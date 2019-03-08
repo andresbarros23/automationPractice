81a7d6916f45406323423561b338cd316699fa55
Andres <abarros@belatrix.com>
2019-03-08 17:32:29 -0300

    Adding Page Factory

---
+++ Classes/Actions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace automationpractice
+{
+    class Actions
+    {
+
+        public static void ClickOn(IWebDriver driver, IWebElement element)
+        {
+            try
+            {
+                element.Click();
+            }
+            catch
+            {
+                IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+                executor.ExecuteScript("arguments[0].click();", element);
+       
+            }
+        }
+
+        public static void Type(IWebDriver driver, IWebElement element, String value)
+        {
+            element.SendKeys(value);
+        }
+
+        public static void SelectElement(IWebDriver driver, SelectElement element, String value)
+        {
+            element.SelectByValue(value);
+        }
+    }
+}
---
+++ Classes/Homepage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace automationpractice.Classes
+{
+    class homepage
+    {
+        private IWebDriver driver;
+
+        public homepage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "contact-link")]
+        private IWebElement contactUsLink;
+        public void clickOnContactLink()
+        {
+            Actions.ClickOn(driver,contactUsLink);
+        }
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+    }
+}
